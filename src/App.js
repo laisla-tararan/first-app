2187835137cb0c8fb60d8713b7b6935de3dd6740
laisla-tararan <laisla.tararan@aluno.senai.br>
2026-05-18 12:59:36 -0300

    aula dia 18/05

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import {View, StyleSheet} from "react-native";
-import ContadorExample from "./hooks/useState_example";
-import TelaDeLogin from "./hooks/useRef_example";
-import TelaMoeda from "./hooks/useEffect_example";
-import Recados from "./hooks/exercicio-recados";
-import FormularioExemplos from "./text_input/formulario";
-import FlatListExemplo from "./flatlist/flatlist";
+import { NavigationContainer } from "@react-navigation/native";
+import StackNavigation from "./navigation/exemplos/stack_navigation";
+import StackNavigator from "./navigation/exemplos/stack_navigation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <FlatListExemplo/>
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 };
 
-  const styles = StyleSheet.create ({
-    container: {
-      gap: 10,
-      padding: 30,
-      flex: 1
-    }
-  });
+const styles = StyleSheet.create ({
+  container: {
+    gap: 10,
+    padding: 30,
+    flex: 1
+  }
+});
