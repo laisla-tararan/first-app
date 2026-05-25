d0c194d352d77d78553c828962a2354d5e265fe5
laisla-tararan <laisla.tararan@aluno.senai.br>
2026-05-25 08:59:26 -0300

    aula dia 25/05

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import {View, StyleSheet} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigation from "./navigation/exemplos/stack_navigation";
 import StackNavigator from "./navigation/exemplos/stack_navigation";
+import BottomTabNavigator from './navigation/exemplos/bottom_tab_navigator';
+import DrawerNavigator from "./navigation/exemplos/drawer_navigation";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <DrawerNavigator/>
     </NavigationContainer>
   );
 };
